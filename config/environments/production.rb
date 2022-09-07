f87ab89fc28dfcb46e7bc1080ff87a8c5014debb
MariamAli-33 <mariam.ali@devsinc.com>
2022-09-07 11:38:54 +0500

    Adding heroku web link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'infinite-chamber-92960.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'test-project-instagram.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
