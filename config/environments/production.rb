973267e5977203989124594be3212996ab256216
MariamAli-33 <mariam.ali@devsinc.com>
2022-09-07 11:50:16 +0500

    Pushing project

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,19 +93,19 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'railsinstagram-321.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'heruko.com',
-    user_name: Rails.application.credentials[:mailer][:username],
-    password: Rails.application.credentials[:mailer][:password],
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
+  # config.active_record.dump_schema_after_migration = false
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = false
+  # config.active_record.dump_schema_after_migration = false
+  # config.action_mailer.default_url_options = { host: 'railsinstagram-321.herokuapp.com' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   port: 587,
+  #   domain: 'heruko.com',
+  #   user_name: Rails.application.credentials[:mailer][:username],
+  #   password: Rails.application.credentials[:mailer][:password],
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true
+  # }
 end
