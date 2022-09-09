bc1c83de2dd3350f46f33ee2eb6ac69b0b0aefef
MariamAli-33 <mariam.ali@devsinc.com>
2022-09-09 12:51:53 +0500

    Pushing final project

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,4 +5,12 @@ class Photo < ApplicationRecord
   belongs_to :photoable, polymorphic: true
   mount_uploader :image, ImageUploader
   validates :image, presence: true
+  validate :image_type
+
+  private
+
+  def image_type
+    errors[:base] << 'you tried uploading wrong file' unless image.content_type.in?(%w[image/png image/jpg image/jpeg
+                                                                                       image/webp])
+  end
 end
